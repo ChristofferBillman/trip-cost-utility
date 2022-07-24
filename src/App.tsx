e53c2a0dcadf1cb0b8957140498243456d67d6b2
ChristofferBillman <christoffer.billman@gmail.com>
2022-07-25 01:27:11 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,57 @@ import './styles/App.css';
 import './styles/Input.css';
 
 import Input from './components/Input';
+import RadioInput from './components/RadioInput';
+import Button from './components/Button';
+
+const fuels: any = {
+	'Car': ['Gasoline', 'Diesel', 'Electricity', 'Ethanol'],
+	'Train': [],
+	'Plane': [],
+	'Bike': [],
+}
 
 export default function App(): JSX.Element {
 
 	const [distance, setDistance] = useState('');
+	const [fuelConsumtpion, setFuelConsumption] = useState('');
+	const [fuel, setFuel] = useState('');
+	const [fuelCost, setfuelCost] = useState('');
+	const [modeOfTransport, setModeOfTransport] = useState('Car');
+
 	return (
 		<div className="App">
 			<div className='app-column'>
-				<h1>Trip Cost Utility</h1>
-				<Input
-					label='Distance'
-					value={distance}
-					onChange={(event: any) => setDistance(event.target.value)}
-				/>
+				<div className='margin-container'>
+					<h1>Trip Cost Utility</h1>
+					<Input
+						label='Distance (km)'
+						value={distance}
+						onChange={(event: any) => setDistance(event.target.value)}
+					/>
+					<RadioInput
+						options={['Car', 'Train', 'Plane', 'Bike']}
+						selectedOption={modeOfTransport}
+						setOption={setModeOfTransport}
+					/>
+					<RadioInput
+						options={fuels[modeOfTransport]}
+						selectedOption={fuel}
+						setOption={setFuel}
+					/>
+					<Input
+						label='Fuel Consumption (L/10km)'
+						value={fuelConsumtpion}
+						onChange={(event: any) => setFuelConsumption(event.target.value)}
+					/>
+					<Input
+						label='Fuel Cost (kr)'
+						value={fuelCost}
+						onChange={(event: any) => setfuelCost(event.target.value)}
+					/>
+
+				</div>
+				<Button text='Calculate' />
 			</div>
 		</div>
 	);
