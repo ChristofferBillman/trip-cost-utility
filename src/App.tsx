02193b804b6d2d2bf5f44ae4eca1b5e0b7ca8806
ChristofferBillman <christoffer.billman@gmail.com>
2022-07-27 23:21:23 +0200

    feat: various changes. see below:
    * correct units now displaying when selecting fuel as electric
    * various style changes
    * calculate button now working
    * added field to enter number of people riding
    * added reset fields button
    * added a small discription under title

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './styles/Input.css';
 import Input from './components/Input';
 import RadioInput from './components/RadioInput';
 import Button from './components/Button';
+import VSpace from './components/VSpace';
 
 const fuels: any = {
 	'Car': ['Gasoline', 'Diesel', 'Electricity', 'Ethanol'],
@@ -19,43 +20,107 @@ export default function App(): JSX.Element {
 	const [distance, setDistance] = useState('');
 	const [fuelConsumtpion, setFuelConsumption] = useState('');
 	const [fuel, setFuel] = useState('');
-	const [fuelCost, setfuelCost] = useState('');
+	const [fuelCost, setFuelCost] = useState('');
 	const [modeOfTransport, setModeOfTransport] = useState('Car');
+
+	const [numOfPeople, setNumOfPeople] = useState('');
+
+	const [currency] = useState('kr');
+	const [result, setResult] = useState('');
 
 	return (
 		<div className="App">
 			<div className='app-column'>
 				<div className='margin-container'>
-					<h1>Trip Cost Utility</h1>
+					<h1>Trip Cost Calculator</h1>
+					<h2>A tool to help you calculate the cost of your travels</h2>
+
+					<VSpace />
+
 					<Input
 						label='Distance (km)'
 						value={distance}
 						onChange={(event: any) => setDistance(event.target.value)}
 					/>
+
+					<VSpace />
+
 					<RadioInput
 						options={['Car', 'Train', 'Plane', 'Bike']}
 						selectedOption={modeOfTransport}
 						setOption={setModeOfTransport}
 					/>
+
+					<VSpace />
+
 					<RadioInput
 						options={fuels[modeOfTransport]}
 						selectedOption={fuel}
 						setOption={setFuel}
 					/>
+
+					<VSpace />
+
 					<Input
-						label='Fuel Consumption (L/10km)'
+						label={fuel === 'Electricity' ? 'Fuel Consumption (kWh/10km)' : 'Fuel Consumption (L/10km)'}
 						value={fuelConsumtpion}
 						onChange={(event: any) => setFuelConsumption(event.target.value)}
 					/>
+
+					<VSpace />
+
 					<Input
-						label='Fuel Cost (kr)'
+						label={fuel === 'Electricity' ? 'Electricty Cost (' + currency + '/kWh)' : 'Fuel Cost (' + currency + '/L)'}
 						value={fuelCost}
-						onChange={(event: any) => setfuelCost(event.target.value)}
+						onChange={(event: any) => setFuelCost(event.target.value)}
+					/>
+
+					<VSpace />
+
+					<Input
+						label={'Number of people'}
+						value={String(numOfPeople)}
+						onChange={(event: any) => setNumOfPeople(event.target.value)}
 					/>
 
 				</div>
-				<Button text='Calculate' />
+
+				<Button
+					text='Calculate'
+					onClick={() => {
+						const cost = parseFloat(distance) / 10 * parseFloat(fuelConsumtpion) * parseFloat(fuelCost)
+						setResult(cost.toString())
+					}}
+				/>
+
+				{result === '' ? '' :
+					<>
+						<div className='margin-container'>
+							<h1>{result + ' ' + currency}</h1>
+							<h2
+								className='black'
+							>
+								{Number(result) / Number(numOfPeople) + ' ' + currency + '/person, split equally'}
+							</h2>
+							{/*<h2>{'Traveling ' + distance + ' km by ' + modeOfTransport + ' using ' + fuel}</h2>*/}
+						</div>
+
+						<Button
+							text='Reset fields'
+							onClick={() => {
+								setDistance('');
+								setFuelConsumption('');
+								setFuelCost('');
+								setModeOfTransport('Car');
+								setFuel('');
+								setNumOfPeople('');
+								setResult('');
+							}}
+							color='#FF2424'
+						/>
+					</>
+				}
 			</div>
-		</div>
+		</div >
 	);
 }
